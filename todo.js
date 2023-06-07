c4bf74fb58294e326de59eb75881ec43304d473c
daaraam <usb_adapter@naver.com>
2023-06-07 21:51:36 +0900

    Feat : todo.js 추가수정

--- todo.js
+++ todo.js
@@ -1,12 +1,15 @@
 function javaGo (){
     const addBtn = document.querySelector('.add-btns')    
-
+    
     function clickInput() {
-
+        // input창이 생기고
+        // if문으로 아무것도 클릭안하면 input창이 안 생기고 클릭하면 생기고 
+        addBtn.innerHTML = `<input type="text" placeholder="할 일을 입력해주세요."/>`
         
     }
-
+    
     addBtn.addEventListener('click',clickInput)
+    
 
 }
 
