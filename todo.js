9e8f12c109668fda8d44a452eb203b8c3a6fc728
daaraam <usb_adapter@naver.com>
2023-06-08 16:00:22 +0900

    feat : css 수정

--- todo.js
+++ todo.js
@@ -1,25 +1,29 @@
 const addBtn = document.querySelector(".add-btns");
 const todoInput = document.querySelector(".input-todo");
 const saves = document.querySelector(".todo-text");
+const inputBtn = document.querySelector(".input-btn");
+// const todoCard = document.querySelector("todo-card");
 
 function clickInput() {
-  //클릭하면, class="add-btns"가 id="hidden"으로 바뀐다 ⓐ
-  //클릭하면, class='sumbit-todo'의 id='hidden'이 삭제된다. ⓑ
   document.querySelector(".add-btns").id = "hidden";
   document.querySelector(".submit-todo").removeAttribute("id");
 }
 
 function saveTodo(event) {
   event.preventDefault();
-  console.log(todoInput.value); //test가 아니라 input한게 입력되도록 고치기 완료!
+  console.log(todoInput.value);
   const newTodo = todoInput.value;
   todoInput.value = "";
-
   document.querySelector(".submit-todo").id = "hidden";
   document.querySelector(".add-btns").removeAttribute("id");
 }
-const inputBtn = document.querySelector(".input-btn");
+
+// function paintTodo(newTodo) {
+//   const li = document.createElement("li");
+//   const span = document.createElement("span");
+//   // todoCard.innerText = newTodo
+//   todoList.appendChild(li);
+// }
 
 addBtn.addEventListener("click", clickInput);
 inputBtn.addEventListener("click", saveTodo);
-// saves.addEventListener('submit',saveTodo);
