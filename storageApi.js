ae65947943b38f9214a35ca0849a000c1f6d9c25
박준영 <pjy93525@gmail.com>
2023-06-08 13:16:16 +0900

    Feat(storageApi작성완료

---
+++ storageApi.js
@@ -0,0 +1,27 @@
+export const getApi = () => {
+  let data = JSON.parse(localStorage.getItem("data"));
+  if (data === null) {
+    localStorage.setItem("data", JSON.stringify([]));
+  }
+  console.log("get완료!", data);
+};
+
+export const postApi = (newContent) => {
+  let data = JSON.parse(localStorage.getItem("data"));
+  const createId = () => {
+    const idDate =
+      `${date.getFullYear()}` +
+      `${date.getMonth() + 1}` +
+      `${date.getDate()}` +
+      `${date.getHours()}` +
+      `${date.getMinutes()}`;
+    const id = Math.floor(Number(idDate) + Math.random() * Number(idDate));
+    return id;
+  };
+
+  const newData = {
+    id: createId(),
+    content: newContent,
+  };
+  data.push(newData);
+};
