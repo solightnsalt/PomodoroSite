f058ad5fba4db7d0b1f72b92656fb94fbb6feb9a
daaraam <usb_adapter@naver.com>
2023-06-08 09:26:33 +0900

    Fix(todo.js)

--- todo.js
+++ todo.js
@@ -1,16 +1,13 @@
-function javaGo (){
-    const addBtn = document.querySelector('.add-btns')    
+// function javaScriptGo (){
+    
+const addBtn = document.querySelector('.add-btns')    
     
     function clickInput() {
-        // input창이 생기고
-        // if문으로 아무것도 클릭안하면 input창이 안 생기고 클릭하면 생기고 
-        addBtn.innerHTML = `<input type="text" placeholder="할 일을 입력해주세요."/>`
-        
+        addBtn.innerHTML = `<input type="text" autofocus placeholder="할 일을 입력해주세요."/>`
     }
     
     addBtn.addEventListener('click',clickInput)
     
 
-}
-
-javaGo();
+// }
+// javaScriptGo();
